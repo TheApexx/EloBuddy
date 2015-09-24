187fcc6ec93137b8f6738c51d265a7b46fa47152
TheApexx <matt18acw@yahoo.com>
2015-09-23 20:50:43 -0700

    In progress.

---
+++ ApexUtility/ApexUtility/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+
+namespace ApexUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
